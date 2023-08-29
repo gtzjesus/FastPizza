50b6f952a9676418c67aff91ff429d3afe818731
gtz.jesus <gtz.jesus@outlook.com>
2023-08-29 15:42:00 -0600

    Fetching Orders, Dynamically calling API

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Error from './ui/Error.jsx';
 import Menu, { loader as menuLoader } from './features/menu/Menu.jsx';
 import Cart from './features/cart/Cart.jsx';
 import CreateOrder from './features/order/CreateOrder.jsx';
-import Order from './features/order/Order.jsx';
+import Order, { loader as orderLoader } from './features/order/Order.jsx';
 import AppLayout from './ui/AppLayout.jsx';
 
 const router = createBrowserRouter([
@@ -34,6 +34,8 @@ const router = createBrowserRouter([
       {
         path: '/order/:orderID',
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
       },
     ],
   },
