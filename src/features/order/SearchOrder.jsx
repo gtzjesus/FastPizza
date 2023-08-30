21a40eacc5299e97c1848acf64415d693987a2a4
gtz.jesus <gtz.jesus@outlook.com>
2023-08-30 14:47:31 -0600

    Styling Form Elements

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -17,6 +17,7 @@ function SearchOrder() {
         placeholder="Search order #"
         value={query}
         onChange={(event) => setQuery(event.target.value)}
+        className="w-28 rounded-full bg-yellow-100 px-4 py-2 text-sm outline-none transition-all duration-300 placeholder:text-stone-500 focus:ring focus:ring-yellow-500 focus:ring-opacity-50 sm:focus:w-72 md:w-64"
       />
     </form>
   );
