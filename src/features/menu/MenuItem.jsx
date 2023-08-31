5348180c4bc8a4a04a5f0d5e8410c88cf16cfdb3
gtz.jesus <gtz.jesus@outlook.com>
2023-08-31 17:24:16 -0600

    Adding Menu Items to the Cart🛒

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,8 +1,25 @@
 import { formatCurrency } from '../../utils/helpers';
 import Button from '../../ui/Button.jsx';
+import { useDispatch } from 'react-redux';
+import { addItem } from '../cart/cartSlice';
+import { useNavigate } from 'react-router-dom';
 
 function MenuItem({ pizza }) {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1,
+    };
+    dispatch(addItem(newItem));
+    // navigate(`/order/${id}`);
+  }
 
   return (
     <li className="flex gap-4 py-2">
@@ -25,7 +42,11 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          <Button type="small">Add to cart</Button>
+          {!soldOut && (
+            <Button type="small" onClick={handleAddToCart}>
+              Add to cart
+            </Button>
+          )}
         </div>
       </div>
     </li>
