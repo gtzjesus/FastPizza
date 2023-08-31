52b8636323ec203095a18be8e46372d811e34a26
gtz.jesus <gtz.jesus@outlook.com>
2023-08-31 14:05:11 -0600

    Styling the Order Form!🧑🏼‍🎨

--- src/UI/Button.jsx
+++ src/UI/Button.jsx
@@ -5,13 +5,13 @@ function Button({ children, disabled, to, type }) {
     'duration-400 inline-block rounded-full bg-yellow-400 px-4 py-3 font-semibold uppercase tracking-wide text-stone-800 transition-colors hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-4 disabled:cursor-not-allowed sm:px-6 sm:py-4';
 
   const base =
-    'duration-400 inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-4 disabled:cursor-not-allowed';
+    'duration-400 text-sm inline-block rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-4 disabled:cursor-not-allowed';
 
   const styles = {
     primary: base + ' md:px-6 md:py-4 px-4 py-3',
     small: base + ' px-4 py-2 md:px-5 md:y-2.5 text-sm',
     secondary:
-      'duration-400 inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors focus:text-stone-800 hover:bg-stone-300 focus:bg-stone-300 hover:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-4 disabled:cursor-not-allowed md:px-6 md:py-2.5 px-4 py-3.5',
+      'duration-400 text-sm inline-block rounded-full border-2 border-stone-300 font-semibold uppercase tracking-wide text-stone-400 transition-colors focus:text-stone-800 hover:bg-stone-300 focus:bg-stone-300 hover:text-stone-800 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-4 disabled:cursor-not-allowed md:px-6 md:py-2.5 px-4 py-3.5',
   };
 
   if (to)
--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -43,29 +43,31 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Les go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Lets go!</h2>
 
       <Form method="POST">
-        <div>
-          <label>Name</label>
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Full Name</label>
           <input className="input" type="text" name="customer" required />
         </div>
 
-        <div>
-          <label>Phone</label>
-
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone</label>
           <input className="input" type="tel" name="phone" required />
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
+          {formErrors?.phone && (
+            <p className="rounde-md mt-2 bg-red-100 p-2 text-xs text-red-700">
+              {formErrors.phone}
+            </p>
+          )}
         </div>
 
-        <div>
-          <label>Address</label>
-
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
           <input className="input" type="text" name="address" required />
         </div>
 
-        <div>
+        <div className="mb-12 flex items-center gap-5">
           <input
             className="h-6 w-6 accent-yellow-400 focus:outline-none focus:ring focus:ring-yellow-400"
             type="checkbox"
@@ -74,7 +76,9 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label htmlFor="priority" className="font-medium">
+            Want to give your order priority?
+          </label>
         </div>
 
         <div>
