14e0d7fd5c6b3354ba2d439eea05cbbd501b8614
gtz.jesus <gtz.jesus@outlook.com>
2023-08-31 17:00:41 -0600

    Storing the store and accessing throughout the whole app

--- src/features/user/CreateUser.jsx
+++ src/features/user/CreateUser.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react';
 import Button from '../../ui/Button.jsx';
+import { useDispatch } from 'react-redux';
+import { updateName } from './userSlice.js';
+import { useNavigate } from 'react-router-dom';
 
 function CreateUser() {
   const [username, setUsername] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!username) return;
+
+    // UPDATE THE STORE TOOLKIT REDUX (dispatch an action)
+    dispatch(updateName(username));
+    navigate('/menu');
   }
 
   return (
